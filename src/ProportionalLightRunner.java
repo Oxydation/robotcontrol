a82b50f77bac61c293feeb88af1fe59805252a3a
Mathias Nigsch <mathias.nigsch@gmail.com>
2015-12-07 13:09:27 +0100

    Fixed

---
+++ src/ProportionalLightRunner.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Mathias on 07/12/2015.
+ */
+public class ProportionalLightRunner extends BaseController {
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    protected void driveRight() {
+
+    }
+
+    @Override
+    protected void driveLeft() {
+
+    }
+
+    @Override
+    protected void driveForward() {
+
+    }
+}
