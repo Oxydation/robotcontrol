007d76c0417ced737b05570a260ca39131e4faac
Mathias Nigsch <mathias.nigsch@gmail.com>
2015-12-14 13:31:24 +0100

    Adjusted runners

--- Webots/Webots_Project/controllers/BangBangLightRunner/BaseController.java
+++ Webots/Webots_Project/controllers/BangBangLightRunner/BaseController.java
@@ -17,8 +17,6 @@ public abstract class BaseController extends DifferentialWheels {
     protected static int S_FRONT_LEFT = 1; // Sensor front left
     protected static int S_FRONT_RIGHT = 2; // Sensor front right
     protected static int S_RIGHT = 3; // Sensor left
-    protected static int S_BACK_LEFT = 4;
-    protected static int S_BACK_RIGHT = 5;
 
     protected DistanceSensor[] _distanceSensors; // Array with all distance _distanceSensors
     protected LightSensor[] _lightSensors;
@@ -27,12 +25,12 @@ public abstract class BaseController extends DifferentialWheels {
         // get distance _distanceSensors and save them in array
         _distanceSensors = new DistanceSensor[]{getDistanceSensor("ps5"),
                 getDistanceSensor("ps7"), getDistanceSensor("ps0"),
-                getDistanceSensor("ps2"),getDistanceSensor("ps4"), getDistanceSensor("ps3")};
+                getDistanceSensor("ps2")};
 
         // get distance _distanceSensors and save them in array
         _lightSensors = new LightSensor[]{getLightSensor("ls5"),
                 getLightSensor("ls7"), getLightSensor("ls0"),
-                getLightSensor("ls2"), getLightSensor("ls4"), getLightSensor("ls3")};
+                getLightSensor("ls2")};
     }
 
     protected void enableLightSensors() {
--- src/ProportionalLightRunner.java
+++ src/ProportionalLightRunner.java
@@ -2,9 +2,27 @@
  * Created by Mathias on 07/12/2015.
  */
 public class ProportionalLightRunner extends BaseController {
+    private static int K_STEERING = 10;
+
     @Override
     public void run() {
+        // Steering angle command = Ksteering * (reference distance – sonar distance)
+        while (step(TIME_STEP) != -1) {
 
+
+            if (_distanceSensors[S_FRONT_LEFT].getValue() > MAX_SENSOR_VALUE
+                    || _distanceSensors[S_LEFT].getValue() > MAX_SENSOR_VALUE) {
+                // drive right - reached a wall
+                driveRight();
+            } else if (_distanceSensors[S_FRONT_RIGHT].getValue() > MAX_SENSOR_VALUE
+                    || _distanceSensors[S_RIGHT].getValue() > MAX_SENSOR_VALUE) {
+                // drive left - reached a wall
+                driveLeft();
+            } else {
+                // drive forward if nothing is in front of the robot
+                driveForward();
+            }
+        }
     }
 
     @Override
