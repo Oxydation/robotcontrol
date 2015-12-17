1a99f421989c473a6310cc41a835d38564da4d1c
jpo2433 <jasmin.pogatschneg@students.fhv.at>
2015-12-17 11:57:58 +0100

    proportional light stopper

--- src/ProportionalLightStopper.java
+++ src/ProportionalLightStopper.java
@@ -4,13 +4,8 @@
 public class ProportionalLightStopper extends ProportionalBaseController {
     private static double[][] controllerMatrix = {
             // FRONT_RIGHT, MIDDLE_RIGHT, RIGHT, BACK_RIGHT, BACK_LEFT, LEFT, MIDDLE_LEFT, FRONT_LEFT
-            // {0, 0, 0, 0.4, 0, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0}, // Motor left
-            // {-1, -1, -1, 0, 0.4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}; // Motor right
-
-
-            {0, 0, 0, 0.4, 0, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0}, // Motor left
-            {-1, -1, -1, 0, 0, 0.4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Motor right
-
+            {0, 0, 0, 0.5, 0, -1, -0.5, -1, 0, 0, 0, 0, 0, 0, 0, 0}, // Motor left
+            {-1, -0.5, -1, 0, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // Motor right
     };
 
     public ProportionalLightStopper() {
@@ -26,12 +21,12 @@ public class ProportionalLightStopper extends ProportionalBaseController {
 
     @Override
     public double[] getConstantVector() {
-        return new double[]{200, 200};
+        return new double[]{0, 0};
     }
 
     @Override
     public double[] getTargetSensorValues() {
-        return new double[]{2500, 0, 0, 0, 0, 0, 0, 2500, 0, 0, 0, 0, 0, 0, 0, 0};
+        return new double[]{-2500, 0, 0, 0, 0, 0, 0, -2500, 0, 0, 0, 0, 0, 0, 0, 0};
     }
 
     public static void main(String[] args) {
